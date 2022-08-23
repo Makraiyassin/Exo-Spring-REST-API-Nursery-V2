1a0d131b4ab189e3c9c96ac6f3733723c01a37fe
Makraiyassin <makrai.yassin@gmail.com>
2022-08-23 11:32:54 +0200

    cors

--- src/main/java/be/bstorm/akimts/rest/bxl/controller/EnfantController.java
+++ src/main/java/be/bstorm/akimts/rest/bxl/controller/EnfantController.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200//")
 @RequestMapping("/enfant")
 public class EnfantController {
 
--- src/main/java/be/bstorm/akimts/rest/bxl/controller/TuteurController.java
+++ src/main/java/be/bstorm/akimts/rest/bxl/controller/TuteurController.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200//")
 @RequestMapping("/tuteur")
 public class TuteurController {
 
